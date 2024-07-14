49592606a2b088622bf990b0b4cb37ab018fe82c
Naim Ahmad <naimahmad201103@gmail.com>
2024-07-14 10:01:26 +0600

    loader fixed

--- src/app/(pages)/(landings)/loading.js
+++ src/app/(pages)/(landings)/loading.js
@@ -1,18 +1,11 @@
-"use client";
+import loader from "@/assets/icons/loader.json";
+import { Lottie } from "@/Components/shared/ClientImport";
 
 export default function Loading() {
   return (
     <div className="preloader">
       <div className="h-full d-flex justify-content-center align-items-center">
-        {/* <LottiePlayer
-          src="https://lottie.host/c590f155-6689-43cd-b9ce-43c78b999e12/UgKnebTk9F.json"
-          background="transparent"
-          speed="1"
-          style={{ width: "200px", height: "200px" }}
-          loop
-          autoplay
-        ></LottiePlayer> */}
-        Loading...
+        <Lottie animationData={loader} loop={true} />
       </div>
     </div>
   );
---
+++ src/app/(pages)/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="preloader">
+      <div className="h-full d-flex justify-content-center align-items-center"></div>
+    </div>
+  );
+}
